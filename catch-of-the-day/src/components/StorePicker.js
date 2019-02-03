3b053e6ea34cbe35fa7da9ff40402c73fd4866b1
Ginger Lee Kretschmer <kretschmons@gmail.com>
2019-02-02 21:50:48 -0800

    add refs to form

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,20 @@ import React from "react"
 import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
+  myInput = React.createRef()
+  goToStore(event) {
+    event.preventDefault()
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
         <input
           type="text"
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
+          ref={this.myInput}
         />
         <button type="submit">Visit Store!</button>
       </form>
