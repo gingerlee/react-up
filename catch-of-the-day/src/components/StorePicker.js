6c60ce8b914de83da5a9acc8b989b7b2de9480e6
Ginger Lee Kretschmer <kretschmons@gmail.com>
2019-02-04 08:40:43 -0800

    add gotostore method

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,8 +3,11 @@ import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
   myInput = React.createRef()
-  goToStore(event) {
+
+  goToStore = event => {
     event.preventDefault()
+    const storeName = this.myInput.value.value
+    this.props.history.push(`/store/${storeName}`)
   }
   render() {
     return (
@@ -12,10 +15,10 @@ class StorePicker extends React.Component {
         <h2>Please enter a store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
-          ref={this.myInput}
         />
         <button type="submit">Visit Store!</button>
       </form>
