48132118fd61733171fe5d1a3b09290af32e091d
Ginger Lee Kretschmer <ginger.kretschmer@instrument.com>
2019-02-09 13:23:21 -0800

    Update order to show fish prices

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -22,6 +22,12 @@ export default class App extends React.Component {
       fishes: sampleFishes
     })
   }
+
+  addToOrder = (key) => {
+    const order = { ...this.state.order}
+    order[key] = order[key] + 1 || 1
+    this.setState({ order: order})
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -29,13 +35,13 @@ export default class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul>
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]}>
+              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}>
                 {key}
               </Fish>
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,14 +4,16 @@ import { formatPrice } from "../helpers"
 export default class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details
+    const isAvailable = status === 'available'
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
         <h3 className="fish-name">
           {name}
-          <span className="price"> {formatPrice(price)}</span>
-          <p>{desc}</p>
+          <span className="price">{formatPrice(price)}</span>
         </h3>
+          <p>{desc}</p>
+          <button onClick={() => {this.props.addToOrder(this.props.index)}} disabled={!isAvailable}>{isAvailable ? 'Add To Cart' : 'Sold out'}</button>
       </li>
     )
   }
